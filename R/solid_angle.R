38e00bb5f9ca9ed38b2735bc5544fb2946a7150f
edwardpmorris <edward.morris@uca.es>
2016-04-28 08:49:25 +0200

    more or less completed vigennette

--- R/solid_angle.R
+++ R/solid_angle.R
@@ -3,7 +3,9 @@
 #' Calculated as:
 #' \deqn{{\Omega} = \pi \times \sin \left( \frac{\theta_{fov}}{2} \times \frac{\pi}{180} \right)^{2}}{Ω = π × sin(θ~fov~/2 × π/180)^2^}
 #' 
-#' Equation derived from \url{http://users.ox.ac.uk/~atdgroup/referencematerial/Notes%20on%20optical%20fibres%20and%20fibre%20bundles.pdf}
+#' Equation derived from \url{http://users.ox.ac.uk/~atdgroup/referencematerial/Notes%20on%20optical%20fibres%20and%20fibre%20bundles.pdf} 
+#' and \url{http://www.oceanopticsbook.info/view/light_and_radiometry/geometry}
+#' 
 #'
 #' @param theta_fov Angular field of view of the instrument light collection apature in degrees (\eqn{\theta _{fov}}, °) 
 #'
@@ -16,7 +18,7 @@ solid_angle <- function(theta_fov){
   # divide by 2 for half-fov
   theta_fov <- theta_fov/2
   # convert to radians
-  #theta_fov <- (theta_fov) * (pi / 180)
+  theta_fov <- (theta_fov) * (pi / 180)
   # calculate 'solid angle' in steradians
   return(
     #pi * (sin(theta_fov))^2
